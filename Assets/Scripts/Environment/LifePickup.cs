edff6f4dbb329b2f7391061e4ba5697b6418ed14
leonardo_f.almeida <leonardo@rgsistemas.com>
2017-03-08 17:56:22 -0300

    Cria blocos e vida

---
+++ Assets/Scripts/Environment/LifePickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePickup : MonoBehaviour {
+
+	public float speed;   
+	private Rigidbody2D body; 
+
+	// Use this for initialization
+	void Start () {
+		body 		= GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate()
+    {
+              
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2 (10f, 0);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        body.AddForce (movement * speed);
+    }
+}
