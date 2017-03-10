c6ea7fd6ce28ca4c8cec9cd4def80680ac75e24c
leonardo_f.almeida <leonardo_f.almeida@hotmail.com>
2017-03-10 20:53:34 -0300

    Alteração em limites da fase, animação de bloco e alteração em script da camera principal

---
+++ Assets/Scripts/Environment/StageLimit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageLimit : MonoBehaviour {
+
+	public float cameraOffset;
+	protected CameraFollowController  _cameraFollow;
+
+	// Use this for initialization
+	void Start () {
+		_cameraFollow = Camera.main.GetComponent<CameraFollowController> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector3 newPosition = GetComponent<BoxCollider2D> ().offset;
+		newPosition.x = _cameraFollow.GetCameraPositionXLimit() - cameraOffset;
+		GetComponent<BoxCollider2D>().offset = newPosition;
+	}
+}
