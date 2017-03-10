4a985ed358bc24b3ef63f064e530e69d8c52345e
leonardo_f.almeida <leonardo_f.almeida@hotmail.com>
2017-03-10 19:16:18 -0300

    Lógica inicial do player - controller e movimento

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	Animator _animator;
+	public Animator playerAnimator
+	{
+		get
+		{
+			if(_animator == null)
+			{
+				_animator = GetComponent<Animator>();
+			}
+
+			return _animator;
+		}
+	}
+
+	SpriteRenderer _spriteRenderer;
+	public SpriteRenderer playerSpriteRenderer
+	{
+		get
+		{
+			if(_spriteRenderer == null)
+			{
+				_spriteRenderer = GetComponent<SpriteRenderer>();
+			}
+
+			return _spriteRenderer;
+		}
+	}
+
+	Rigidbody2D _rigidBody;
+	public Rigidbody2D playerRigidBody
+	{
+		get
+		{
+			if(_rigidBody == null)
+			{
+				_rigidBody = GetComponent<Rigidbody2D>();
+			}
+
+			return _rigidBody;
+		}
+	}
+		
+	void Start()
+	{
+		if(GetComponent<Animator>() == null)
+		{
+			Debug.LogError("Get Animator error");
+		}
+		else
+		{
+			_animator = GetComponent<Animator>();
+		}
+
+		if (GetComponent<SpriteRenderer>() == null)
+		{
+			Debug.LogError("Get spriteRenderer error");
+		}
+		else
+		{
+			_spriteRenderer = GetComponent<SpriteRenderer>();
+		}
+
+		if (GetComponent<Rigidbody2D>() == null)
+		{
+			Debug.LogError("Get RigidBody2D error");
+		}            
+		else
+		{
+			_rigidBody = GetComponent<Rigidbody2D>();
+		}
+			
+	}
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,86 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : PlayerController {
 
 
     public float speed;             //Floating point variable to store the player's movement speed.
-    Animator animator;
-    SpriteRenderer sprite;
-    private Rigidbody2D body;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
     private bool isGrounded;
     
-	// Use this for initialization
-	void Start () {
-		body 		= GetComponent<Rigidbody2D>();
-		animator 	= GetComponent<Animator>();
-		sprite 		= GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	 //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+	 //FixedUpdate is called at a fixed interval and is independent of frame rate.
     void FixedUpdate()
     {
-        //Store the current vertical input in the float moveVertical.
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        
-        if(Input.GetKeyDown("space"))
-        {
-            if(isGrounded)
-            {
-                animator.SetBool("isJump",true);
-                
-                Vector2 jumpVector = new Vector2 (0, 200);
-
-                body.AddForce (jumpVector);  
-
-                isGrounded = false;
-            }
-        	
-        }	
-
-        if(moveHorizontal != 0)
-        {
-        	animator.SetBool("isRunning",true);	
-        }
-        else
-        {
-        	animator.SetBool("isRunning",false	);		
-        }
-        
-
-        if(moveHorizontal < 0)
-        {
-        	sprite.flipX = true;
-        }
-        
-		if(moveHorizontal > 0)
-        {
-        	sprite.flipX = false;	
-        }
-
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2 (moveHorizontal, 0);
-
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        body.AddForce (movement * speed);
+		Move ();
+		Jump ();
     }
+
+	void Move()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		if(moveHorizontal != 0)
+		{
+			playerAnimator.SetBool("isRunning",true);	
+		}
+		else
+		{
+			playerAnimator.SetBool("isRunning",false	);		
+		}
+
+
+		if(moveHorizontal < 0)
+		{
+			playerSpriteRenderer.flipX = true;
+		}
+
+		if(moveHorizontal > 0)
+		{
+			playerSpriteRenderer.flipX = false;	
+		}
+
+		Vector2 movement = new Vector2 (moveHorizontal, 0);
+
+		playerRigidBody.AddForce (movement * speed);
+	}
+
+	void Jump()
+	{
+		if(Input.GetKeyDown("space"))
+		{
+			if(isGrounded)
+			{
+				playerAnimator.SetBool("isJump",true);
+
+				Vector2 jumpVector = new Vector2 (0, 200);
+
+				playerRigidBody.AddForce (jumpVector);  
+
+				isGrounded = false;
+			}
+		}	
+	}
+
+
 
      void OnCollisionEnter2D(Collision2D coll) 
      {
         if (coll.gameObject.tag == "ground")
         {
-                isGrounded = true;
-                animator.SetBool("isJump",false);
+            isGrounded = true;
+			playerAnimator.SetBool("isJump",false);
         }
         if (coll.gameObject.tag == "Enemy")
         {
                 isGrounded = true;
-                animator.SetBool("isJump",false);
+			playerAnimator.SetBool("isJump",false);
         }
     }
 }
